96b0e36b1934c9d3905a8cdb3cccdddfbbd837ae
Andrea Tredrea <andrea.tredrea@gmail.com>
2021-04-12 15:59:51 +0200

    CRUD from the back-end

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,22 +3,31 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
+require("@rails/ujs").start()
+
 require("@rails/ujs").start()
 
 require("turbolinks").start()
+
 require("@rails/activestorage").start()
+
 require("channels")
 
-//= require materialize
+require("jquery")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import '../css/application'
 
+import 'materialize-css/dist/js/materialize'
+
+import 'material-design-icons'
+
+//$( document ).ready(function) block
 
 $(document).on('turbolinks:load', function () {
+
     $(".dropdown-trigger").dropdown();
-});
+
+})
+
